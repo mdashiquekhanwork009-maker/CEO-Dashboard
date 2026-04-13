1a0192628616e8e46c3d6c555fa87291ded0823e
mdashiquekhanwork009-maker <mdashiquekhanwork009@gmail.com>
2026-04-13 11:34:43 +0530

    Auto-sync from OneDrive

---
+++ data/onboarding.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+try:
+    source_path = 'C:/Data/selection.csv'
+    target_path = 'C:/Users/E36250444/OneDrive - JoulestoWatts Business Solutions Pvt Ltd/Desktop/Streamlit Dashboard/data/selection.csv'
+
+    source_df = pd.read_csv(source_path)
+    source_df.to_csv(target_path, index=False)
+
+except Exception as e:
+    with open("error_log.txt", "a") as f:
+        f.write(str(e) + "\n")
