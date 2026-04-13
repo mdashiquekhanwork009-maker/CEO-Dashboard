3f02e20df8a4aa8968e54698ee05a35f97f1be9b
mdashiquekhanwork009-maker <mdashiquekhanwork009@gmail.com>
2026-04-13 11:38:57 +0530

    Auto-sync from OneDrive

--- data/exit_pipeline.py
+++ data/exit_pipeline.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 try:
-    source_path = 'C:/Data/exit.csv'
-    target_path = 'C:/Users/E36250444/OneDrive - JoulestoWatts Business Solutions Pvt Ltd/Desktop/Streamlit Dashboard/data/exit.csv'
+    source_path = 'C:/Data/exit_pipeline_data.csv'
+    target_path = 'C:/Users/E36250444/OneDrive - JoulestoWatts Business Solutions Pvt Ltd/Desktop/Streamlit Dashboard/data/exit_pipeline_data.csv'
 
     source_df = pd.read_csv(source_path)
     source_df.to_csv(target_path, index=False)
