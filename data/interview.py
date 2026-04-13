b7de389a35e495b9381f05f5dfad89cdf8978db9
mdashiquekhanwork009-maker <mdashiquekhanwork009@gmail.com>
2026-04-13 11:20:11 +0530

    Auto-sync from OneDrive

--- data/interview.py
+++ data/interview.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 try:
-    source_path = 'C:/Data/submission.csv'
-    target_path = 'C:/Users/E36250444/OneDrive - JoulestoWatts Business Solutions Pvt Ltd/Desktop/Streamlit Dashboard/data/submission.csv'
+    source_path = 'C:/Data/interview.csv'
+    target_path = 'C:/Users/E36250444/OneDrive - JoulestoWatts Business Solutions Pvt Ltd/Desktop/Streamlit Dashboard/data/interview.csv'
 
     source_df = pd.read_csv(source_path)
     source_df.to_csv(target_path, index=False)
