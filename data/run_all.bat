132d550971c00b225dce42facd4456dfcdafa82a
mdashiquekhanwork009-maker <mdashiquekhanwork009@gmail.com>
2026-04-13 11:50:23 +0530

    Auto-sync from OneDrive

---
+++ data/run_all.bat
@@ -0,0 +1,16 @@
+@echo off
+
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\demand.py"
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\submission.py"
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\interview.py"
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\selection.py"
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\selction_pipeline.py"
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\onboarding.py"
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\exit.py"
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\exit_pipeline.py"
+start /min python "C:\Users\E36250444\OneDrive - JoulestoWatts Business Solutions Pvt Ltd\Desktop\Streamlit Dashboard\data\activeHeadCount.py"
+
+
+
+
+exit
