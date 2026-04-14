8db7fd1adc0d19544cd355e9654ee86fc8f7ec05
mdashiquekhanwork009-maker <mdashiquekhanwork009@gmail.com>
2026-04-14 18:20:54 +0530

    Auto-sync from OneDrive

--- data/exit.py
+++ data/exit.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 try:
     source_path = 'C:/Data/exit.csv'
-    target_path = 'C:/Users/E36250444/OneDrive - JoulestoWatts Business Solutions Pvt Ltd/Desktop/Streamlit Dashboard/data/exit.csv'
+    target_path = 'C:/Users/E36250444/OneDrive - JoulestoWatts Business Solutions Pvt Ltd/Desktop/Streamlit Dashboard/data/exit_data.csv'
 
     source_df = pd.read_csv(source_path)
     source_df.to_csv(target_path, index=False)
